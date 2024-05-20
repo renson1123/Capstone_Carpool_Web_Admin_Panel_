1a6d2abb9acbd41a9deea083d5d88d81af7188db
Renson Placino <rensplaci1123@gmail.com>
2024-05-20 22:57:44 +0800

    Implementing headers for Drivers data

--- lib/pages/drivers_page.dart
+++ lib/pages/drivers_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class DriversPage extends StatefulWidget
@@ -11,17 +13,74 @@ class DriversPage extends StatefulWidget
 
 class _DriversPageState extends State<DriversPage>
 {
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Drivers Page",
-          style: TextStyle(
-              color: Colors.greenAccent,
-              fontSize: 24
+  Widget header(int headerFlexValue, String headerTitle)
+  {
+    return Expanded(
+      flex: headerFlexValue,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black),
+          color: Colors.green.shade500,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Text(
+            headerTitle,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
           ),
         ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+                Container(
+                  alignment: Alignment.topLeft,
+                  child: const Text(
+                    "Manage Drivers",
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+
+                const SizedBox(
+                  height: 18,
+                ),
+
+                Row(
+                  children: [
+                    header(1, "Driver ID"),
+                    header(1, "FName"),
+                    header(1, "MName"),
+                    header(1, "LName"),
+                    header(1, "Phone"),
+                    header(1, "Email"),
+                    header(1, "Employee #"),
+                    header(1, "Driver Lic (F)"),
+                    header(1, "Driver Lic (B)"),
+                    header(1, "Med Cert"),
+                    header(1, "Action"),
+                  ],
+                ),
+
+                // Display Data
+
+              ],
+            ),
+          ),
       ),
     );
   }
