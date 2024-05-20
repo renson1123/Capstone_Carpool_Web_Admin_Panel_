0abf242c3404225f26063520af6854fbc0dfd8bd
Renson Placino <rensplaci1123@gmail.com>
2024-05-20 16:15:47 +0800

    Creating Web Admin Panel

---
+++ lib/dashboard/side_navigation_drawer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SideNavigationDrawer extends StatefulWidget {
+  const SideNavigationDrawer({super.key});
+
+  @override
+  State<SideNavigationDrawer> createState() => _SideNavigationDrawerState();
+}
+
+class _SideNavigationDrawerState extends State<SideNavigationDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
